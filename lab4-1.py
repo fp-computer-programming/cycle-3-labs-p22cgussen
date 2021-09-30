b7a1732e94bc0ebb1595371776e7c1fc09ff0715
p22cgussen <p22cgussen@fairfieldprep.org>
2021-09-30 11:06:21 -0400

    edit lab 4-1

---
+++ lab4-1.py
@@ -0,0 +1,6 @@
+# author: CCG 9/30/21
+
+if magic_charge < 90 or shield_charge < 75: 
+    print ("The dragon burns you to a crisp.")
+else:
+    print ("You defeated the dragon! But the princess is in another castle.")
