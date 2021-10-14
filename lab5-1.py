df92f3bce3a692f8edc8dff1a0924ec44f2a5bc7
p22cgussen <p22cgussen@fairfieldprep.org>
2021-10-14 09:36:09 -0400

    complete lab5-1

---
+++ lab5-1.py
@@ -0,0 +1,16 @@
+# Author CCG 10/4/21
+
+# int(a)
+# NameError
+
+# int('a')
+# Value Error
+
+# 'a' + 2
+# TypeError
+
+# import date
+# ModuleNotFoundError
+
+# print("I'm a happy camper!)
+# SyntaxError
